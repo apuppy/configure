1123a5aa69b6aedaa320e86b975ac3ac9afd2404
lihongde <lihongde@leweikeji.com>
2020-04-22 17:48:17 +0800

    docker redis

--- docker-containers/redis/command.sh
+++ docker-containers/redis/command.sh
@@ -1,1 +1,3 @@
-docker run --name some-redis -v /data/redis:/data  -d redis redis-server --appendonly yes
+# change $HOME/data/redis to the folder path where you would like in host
+# port mapping 6379
+docker run --name some-redis -v $HOME/data/redis:/data -p 6379:6379 -d redis redis-server --appendonly yes
