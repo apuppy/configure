66228e906a20d0f5cfd3ab9e3d5d8640c896ca92
lihongde <chinalihongde@gmail.com>
2023-01-16 09:52:04 +0800

    tidy repo files

--- docker-containers/redis/command.sh
+++
@@ -1,3 +0,0 @@
-# change $HOME/data/redis to the folder path where you would like in host
-# port mapping 6379
-docker run --name some-redis -v $HOME/data/redis:/data -p 6379:6379 -d redis redis-server --appendonly yes
