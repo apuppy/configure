83d83bf5da74b4317369251fad56973c747d4f33
lihongde <chinalihongde@gmail.com>
2023-01-19 15:35:13 +0800

    feat: sync_php_repos.sh optimization

--- git/sync_php_repos.sh
+++ git/sync_php_repos.sh
@@ -1,27 +1,37 @@
+#!/bin/bash
+
 repos=(
-"advert-service"
-"authority-service"
-"backend-service"
-"common-service"
-"crontab-messages"
-"crontab-service"
-"file-service"
-"job_commom"
-"job-business"
-"login-service"
-"log-service"
-"marketing-service"
-"message-service"
-"pay-service"
-"recruitment_main_station"
-"risk-service"
-"user-data"
-"user-service"
+	"advert-service"
+	"authority-service"
+	"backend-service"
+	"common-service"
+	"crontab-messages"
+	"crontab-service"
+	"file-service"
+	"job_commom"
+	"job-business"
+	"login-service"
+	"logger"
+	"log-service"
+	"marketing-service"
+	"message-service"
+	"pay-service"
+	"recruitment_main_station"
+	"risk-service"
+	"user-data"
+	"user-service"
 )
 
-for repo in ${repos[@]}
-do
-	full_repo_path="/Users/hongde/code/work/php/$repo"
+repo_parent_dir="/Users/hongde/code/work/php/"
+
+for repo in ${repos[@]}; do
+	full_repo_path=$repo_parent_dir$repo
 	echo $full_repo_path
-	cd $full_repo_path && git pull && git checkout prod
+	if [[ $repo = "logger" ]]; then
+		cd $full_repo_path && git pull && git checkout release-2.0
+	elif [[ $repo = "crontab-message" ]]; then
+		cd $full_repo_path && git pull && git checkout master
+	else
+		cd $full_repo_path && git pull && git checkout prod
+	fi
 done
